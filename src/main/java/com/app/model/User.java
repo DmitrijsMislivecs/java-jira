77b15cab11b9091aa197245a91556070a1a02489
dms493 <dimis@inbox.lv>
2023-11-09 19:02:02 +0200

    Initial commit

---
+++ src/main/java/com/app/model/User.java
@@ -0,0 +1,58 @@
+package com.app.model;
+
+public class User {
+    private long id;
+    private String name;
+    private String surname;
+    private String email;
+    private long roleId;
+    private String photoUrl;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+}
