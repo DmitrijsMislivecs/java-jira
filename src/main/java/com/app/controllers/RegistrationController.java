fc1cc7e7ea24a40e697524712f7a2d81868bb99f
dms493 <dimis@inbox.lv>
2023-12-13 23:31:12 +0200

    Creating controllers and first html pages

---
+++ src/main/java/com/app/controllers/RegistrationController.java
@@ -0,0 +1,25 @@
+package com.app.controllers;
+
+import com.app.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class RegistrationController {
+    @GetMapping("/registration")
+    public String getRegistrationForm(Model model) {
+        model.addAttribute("newUser", new User());
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String saveNewUser(@ModelAttribute User user, Model model) {
+        model.addAttribute("firstName", user.getFirstName());
+        model.addAttribute("lastName", user.getLastName());
+
+        return "successRegistration";
+    }
+}
